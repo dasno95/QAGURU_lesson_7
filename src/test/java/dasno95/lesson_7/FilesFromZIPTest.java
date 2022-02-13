6a8b4b15e9fc19e6c27ab65730679eb64ca971ec
dasno95 <dasno95@gmail.com>
2022-02-14 03:48:49 +0400

    FilesFromZIPTest.java updated build.gradle updated

--- src/test/java/dasno95/lesson_7/FilesFromZIPTest.java
+++ src/test/java/dasno95/lesson_7/FilesFromZIPTest.java
@@ -1,6 +1,7 @@
 package dasno95.lesson_7;
 
 import com.codeborne.pdftest.PDF;
+import com.codeborne.xlstest.XLS;
 import org.junit.jupiter.api.Test;
 
 import java.io.InputStream;
@@ -23,7 +24,16 @@ public class FilesFromZIPTest {
         }
 
         // xlsx
+        ZipEntry zipEntryXLSX = zip.getEntry("XLSX_file.xlsx");
+        try(InputStream isXLSX = zip.getInputStream(zipEntryXLSX)) {
+            XLS parsed = new XLS(isXLSX);
+            assertThat(parsed.excel.getSheetAt(0).getRow(0).getCell(0).getStringCellValue()).isEqualTo("Teacher");
+            assertThat(parsed.excel.getSheetAt(0).getRow(0).getCell(1).getStringCellValue()).isEqualTo("Office");
+            assertThat(parsed.excel.getSheetAt(0).getRow(1).getCell(0).getStringCellValue()).isEqualTo("Ivanov");
+            assertThat(parsed.excel.getSheetAt(0).getRow(1).getCell(1).getNumericCellValue()).isEqualTo(305.0);
+        }
 
         //csv
+
     }
 }
